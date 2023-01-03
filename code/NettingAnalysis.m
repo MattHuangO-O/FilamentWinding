327af8a3941f0b65fff7ba41e049f54c3784daad
Matthew Huang <myh15@scarletmail.rutgers.edu>
2023-01-03 12:01:16 -0500

    modified netting analysis file only

--- code/NettingAnalysis.m
+++ code/NettingAnalysis.m
@@ -11,7 +11,7 @@ t = .004;%filament thickness
 r_0 = .5;%starting radius
 n_pts = 250; %number of points
 R = D/2; %Radius 
-
+V_f = .65; %volume fraction
 S_D = S_F / N; %Design strength
 
 N_hoop = P * R; %hoop force
@@ -25,15 +25,26 @@ for i = 1:length(alpha)
     t_hoop(i) = (N_hoop - N_axial * tan(alpha(i)*toRad)^2) / S_D; %helical thickness
 end
 
-t_total = t_helical + t_hoop;
+t_total = (t_helical + t_hoop) / V_f;
 
+n_hoop = ceil(t_hoop /  t); %number of hoop layers rounded up
+n_helical = ceil(t_helical / t); %number of helical layers
+
+
+yyaxis left
 plot(alpha, t_helical, 'LineWidth', 2);
 hold on
 plot(alpha, t_hoop, 'LineWidth', 2);
-plot(alpha, t_total, 'LineWidth', 2);
-title('Layer Thickness v.s Wind Angle')
-legend('Helical', 'Hoop')
+title('Helical and Hoop Layers for Various Wind Angles')
 xlabel('Wind Angle (Degrees)')
 ylabel('Thickness (in)')
 ylim([0 .04])
-xlim([0 58])
+
+yyaxis right
+plot(alpha, n_hoop, 'LineWidth', 2);
+plot(alpha, n_helical, 'LineWidth', 2);
+xlabel('Wind Angle (Degrees)')
+ylabel('Number of Layers')
+xlim([0 54.7])
+legend('helical thickness' , 'hoop thickness', 'helical layers', 'hoop layers')
+hold off
